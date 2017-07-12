ab951faf4b3f52c2edd1ed8b3b645cb216f6a58b
Gustavo Félix <gfelixcabello@gmail.com>
2017-07-12 09:17:30 -0500

    update 11 07

---
+++ shopy2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gfeli\desktop\python\django_test\django_test\shopy2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
