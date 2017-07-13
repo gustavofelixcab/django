fe66bcc9a09fe9d46baf382c8f780d45796c149a
Gustavo Félix <gfelixcabello@gmail.com>
2017-07-13 08:31:48 -0500

    cambios 13-07

--- Shoppy/products/views.py
+++ Shoppy/products/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.template import loader
+from .models import Product
 # Create your views here.
+
+def hello_world(request):
+    #return HttpResponse('hello_world')
+    product = Product.objects.order_by('id')
+    template = loader.get_template('index.html')
+
+    context = {
+        'product': product
+    }
+
+    return HttpResponse(template.render(context, request))
+    
